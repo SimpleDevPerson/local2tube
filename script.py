3918c16d472694b17db4a3a29c8542d25c229f61
Alessandro Iuliano <iulalex0301@gmail.com>
2024-01-11 21:37:30 +0100

    more things i guess

--- script.py
+++ script.py
@@ -32,9 +32,12 @@ def main():
     curses.init_pair(3, curses.COLOR_GREEN, 0)
     curses.init_pair(4, curses.COLOR_RED, 0)
 
+    song_number = 0
 
     for filename in filenames:
         if not filename.endswith(".lrc") and not filename.endswith(".txt"):
+            song_number += 1
+            total_songs = len(filenames)
             search_results = ytmusic.search(query=filename, filter="songs", limit=1)
             f = open("risultato.txt", "w")
             f.write(str(search_results).encode("cp1252", errors="replace").decode("cp1252"))
@@ -53,7 +56,9 @@ def main():
             oScreen.addstr("/", curses.A_BOLD)
             oScreen.addstr("n" + "\n", curses.color_pair(4))
             oScreen.addstr("\n")
-            oScreen.addstr("Added songs:" + str(songs_add_list).encode("cp1252", errors="replace").decode("cp1252") + "\n")
+            oScreen.addstr("Song number: " + str(song_number) + "/" + str(total_songs) + "\n")
+            song_string = str(songs_add_list).encode("cp1252", errors="replace").decode("cp1252")
+            oScreen.addstr("Added songs:" + "\n" + str(songs_add_list).encode("cp1252", errors="replace").decode("cp1252")[len(str(songs_add_list)) - 100:] + "\n")
             
             oEvent = oScreen.getch()
             if oEvent == ord("y"):
